5b7db9ce86d138e05bab23a713bd00b890ad22a3
Sayeeda Saima Akhtar Rafia <133207591+saimarafia98@users.noreply.github.com>
2023-06-05 15:04:22 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+console.log(<div id="loginform" class="loginpage">
+			<div class="formbox">
+				<div class="btnbox">
+					<div id="btn"></div>
+					<button class="toggle-btn">Log in</button>
+					<button class="toggle-btn">Register</button>
+				</div>
+				<form action="" id="login" class="input-group-login">
+					<input type="Email" class="inputfield" placeholder="Enter Your Email" required>
+					<input type="text" class="inputfield" placeholder="User Name" required>
+					<input type="text" class="inputfield" placeholder="Password" required>
+					<input type="checkbox" class="check-box"><span>Remember me</span>
+					<button type="Submit" class="submit-btn">Log in</button>
+				</form>
+				<form action="" id="Register" class="input-group-register">
+					<input type="text" class="inputfield" placeholder="First Name" required>
+					<input type="text" class="inputfield" placeholder="Last Name" required>
+					<input type="Email" class="inputfield" placeholder="Enter Your Email" required>
+					<input type="text" class="inputfield" placeholder="User Name" required>
+					<input type="Password" class="inputfield" placeholder="Enter Password" required>
+					<input type="Password" class="inputfield" placeholder="Confirm Password" required>
+					<input type="checkbox" class="check-box"><span>I agree to the <a href="">Terms & Conditions.</a></span>
+					<button type="Submit" class="submit-btn">Register</button>
+				</form>
+
+			</div>
+		</div>
+	</div>)        
+
